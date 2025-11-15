71b14c795f24f08697b914832dc1f9a4b257aaf0
TheTaver <connorldickson@gmail.com>
2025-11-15 10:37:45 -0800

    button themes

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ pygame.init()
 
 pygame.display.set_caption('Quick Start')
 
-resolution = (1600, 900)
+resolution = (1280, 720)
 window_surface = pygame.display.set_mode(resolution)
 
 
 background = pygame.Surface(resolution)
-background.fill(pygame.Color("#000000"))
+background.fill(pygame.Color("#424242"))
 
-manager = pygame_gui.ui_manager.UIManager(resolution)
+manager = pygame_gui.ui_manager.UIManager(resolution, 'theme.json')
 
 hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
                                              text='Say Hello',
                                              manager=manager)
+
+button_size = ((resolution[0]-0.25)/4, resolution[1]*0.2)
+button_height = resolution[1]*0.775
+spell_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((resolution[0]*0.05, button_height) , button_size),
+                                            text='Spell 1',
+                                            object_id='spell_button',
+                                            manager=manager)
 
 clock = pygame.time.Clock()
 is_running = True
